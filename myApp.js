8d8ce68fbd806a89b904acbd62c1114033a3acc9
Yurii Nebos <yurii.nebos@avenga.com>
2023-09-22 12:49:19 +0200

    Another one

--- myApp.js
+++ myApp.js
@@ -1,15 +1,11 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
 
-app.get('/', (req, res) => res.send('Hello Express'));
+app.use('/public', express.static(__dirname + '/public'));
 
-// function handler(req, res) {
-//     return res.send('Hello Express');
-// }
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
-// app.get('/', handler);
 
 
 
