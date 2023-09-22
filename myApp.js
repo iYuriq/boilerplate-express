91e40f71511eb9cae4c74a86d99a45d5b6fcbb9e
Yurii Nebos <yurii.nebos@avenga.com>
2023-09-22 12:07:29 +0200

    Added app.get() method

--- myApp.js
+++ myApp.js
@@ -3,10 +3,13 @@ let app = express();
 
 console.log("Hello World");
 
+app.get('/', (req, res) => res.send('Hello Express'));
 
+// function handler(req, res) {
+//     return res.send('Hello Express');
+// }
 
-
-
+// app.get('/', handler);
 
 
 
